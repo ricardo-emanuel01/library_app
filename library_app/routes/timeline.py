bd67b00a052a36ff252683f6b3b3765b2bf5aea0
Ricardo Emanuel <r200518@dac.unicamp.br>
2023-10-29 10:40:51 -0300

    :sparkles: Adicionando arquivo que vai comportar as rotas referente as geracoes de timeline de cada usuario

---
+++ library_app/routes/timeline.py
@@ -0,0 +1,41 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query
+
+from library_app.database import booksdb
+from library_app.models import User
+from library_app.schemas import BookPublic
+from library_app.security import get_current_user
+
+router = APIRouter(prefix='/timeline', tags=['timeline'])
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
+
+@router.get('/', response_model=list[BookPublic])
+def get_timeline(
+    user: CurrentUser,
+    skip: Annotated[int, Query(ge=0)] = 0,
+    limit: Annotated[int, Query(ge=1)] = 10,
+):
+    genre1 = user.genre1
+    genre2 = user.genre2
+
+    query = {
+        '$and': [
+            {'$or': [{'genre': genre1}, {'genre': genre2}]},
+            {'user_id': {'$ne': user.id}},
+        ]
+    }
+
+    books = list(booksdb.find(query, skip=skip, limit=limit))
+
+    if not books:
+        books = list(booksdb.find({}, skip=skip, limit=limit))
+
+    for book in books:
+        str_id = str(book['_id'])
+        book.pop('_id')
+        book['id'] = str_id
+
+    return books
