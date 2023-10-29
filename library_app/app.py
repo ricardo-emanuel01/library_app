71d750fcfc65a4d7845c9ef9cb613cadbf68b98d
Ricardo Emanuel <r200518@dac.unicamp.br>
2023-10-29 18:00:55 -0300

    Adcionando redirecionando para docs na rota raiz

--- library_app/app.py
+++ library_app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from library_app.routes import auth, books, timeline, users
 
@@ -8,3 +9,8 @@ app.include_router(auth.router)
 app.include_router(books.router)
 app.include_router(timeline.router)
 app.include_router(users.router)
+
+
+@app.get('/')
+def root():
+    return RedirectResponse(url='/docs')
