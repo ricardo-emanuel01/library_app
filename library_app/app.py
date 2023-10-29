1069233888809ecfed1ee0cbdfc43ed80d5e5d00
Ricardo Emanuel <r200518@dac.unicamp.br>
2023-10-29 10:41:18 -0300

    :sparkles: Adicionando novo router de timeline na aplicacao

--- library_app/app.py
+++ library_app/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 
-from library_app.routes import auth, books, users
+from library_app.routes import auth, books, timeline, users
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(books.router)
+app.include_router(timeline.router)
 app.include_router(users.router)
