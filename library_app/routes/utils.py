760cc7b09589130b6e1830804cdab52cdba34cf6
Ricardo Emanuel <r200518@dac.unicamp.br>
2023-10-30 13:12:06 -0300

    Abstraindo a construcao de uma oferta de troca

--- library_app/routes/utils.py
+++ library_app/routes/utils.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from typing import Annotated
 
 from fastapi import Depends, Query
 
 from library_app.models import User
+from library_app.schemas import StatusTrade
 from library_app.security import get_current_user
 
 CurrentUser = Annotated[User, Depends(get_current_user)]
@@ -15,3 +17,15 @@ Author = Annotated[str | None, Query(max_length=30, alias='author name')]
 Skip = Annotated[int, Query(ge=0)]
 Limit = Annotated[int, Query(ge=1)]
 OfferId = Annotated[str, Query()]
+
+
+def build_trade_offer(user_id: int, offer: dict):
+    now = datetime.now()
+    offer['status'] = StatusTrade.awaiting_approval
+    offer['sender_id'] = user_id
+    offer['year'] = now.year
+    offer['month'] = now.month
+    offer['day'] = now.day
+    offer['hour'] = now.hour
+
+    return offer
