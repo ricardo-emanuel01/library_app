d290896c5ea7387d4f67852600d7553358ebde37
Ricardo Emanuel <r200518@dac.unicamp.br>
2023-10-30 13:15:30 -0300

    :sparkles: Adicionando router das notificacoes na aplicacao

--- library_app/app.py
+++ library_app/app.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-from library_app.routes import auth, books, timeline, trade, users
+from library_app.routes import auth, books, notify, timeline, trade, users
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(books.router)
+app.include_router(notify.router)
 app.include_router(timeline.router)
 app.include_router(trade.router)
 app.include_router(users.router)
